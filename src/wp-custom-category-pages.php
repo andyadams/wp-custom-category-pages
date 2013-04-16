789b820e9ffe23731c53bce057b230d168412572
Andy Adams <aadams@jestro.com>
2013-04-16 13:40:44 -0700

    Adding readme and fixing email.

--- src/wp-custom-category-pages.php
+++ src/wp-custom-category-pages.php
@@ -9,7 +9,7 @@ Author URI: http://geoffkenyon.com
 License: GPL2
 */
 
-/*  Copyright 2013  Geoff Kenyon  (email : )
+/*  Copyright 2013  Geoff Kenyon  (email: geoff@geoffkenyon.com)
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License, version 2, as
